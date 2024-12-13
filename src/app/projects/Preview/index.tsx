eebfcd46525329d3d62eb5a057d428a3e208d529
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2024-12-13 18:48:12 +0100

    add: fix

--- src/app/projects/Preview/index.tsx
+++ src/app/projects/Preview/index.tsx
@@ -15,11 +15,10 @@ const ProjectPreview = ({
   selectedProject,
   closePreview,
 }: ProjectPreviewProps) => {
-  console.log(selectedProject);
-
   return (
     <div>
       <div
+        onClick={closePreview}
         className={cn(
           "fixed top-0 left-0 transition-all duration-300 bg-black/80 w-full h-screen z-50",
           isPreviewOpen ? "opacity-100 visible " : "opacity-0 invisible "
@@ -27,7 +26,7 @@ const ProjectPreview = ({
       ></div>
       <div
         className={cn(
-          "sm:w-[80vw] w-[95vw] transition-all duration-500  grid grid-cols-[auto_1fr] p-1 bg-white dark:bg-zinc-900 h-[90vh] fixed top-1/2 left-1/2 z-50 rounded-lg -translate-y-1/2 -translate-x-1/2",
+          "sm:w-[80vw] w-[95vw] transition-all duration-500  grid lg:grid-cols-[auto_1fr] grid-cols-1 p-1 overflow-y-auto bg-white dark:bg-zinc-900 h-[90vh] fixed top-1/2 left-1/2 z-50 rounded-lg -translate-y-1/2 -translate-x-1/2",
           isPreviewOpen
             ? "opacity-100 visible scale-100"
             : "opacity-0 invisible scale-95"
@@ -35,13 +34,13 @@ const ProjectPreview = ({
       >
         <button
           onClick={closePreview}
-          className="bg-white/85 size-[2rem] grid place-items-center rounded-full absolute -right-10 top-0"
+          className="bg-white/85 size-[2rem] ms-auto grid place-items-center rounded-full lg:absolute -right-10  top-0"
         >
           <X />
         </button>
         {selectedProject && (
           <>
-            <div className="w-[20rem] px-3  py-3 overflow-y-auto">
+            <div className="lg:w-[20rem] w-full px-3  py-3 lg:overflow-y-auto">
               <h1 className="font-semibold text-2xl capitalize">
                 {selectedProject?.title}
               </h1>
@@ -81,9 +80,10 @@ const ProjectPreview = ({
                 </div>
               </div>
             </div>
-            <div className="overflow-y-auto">
+
+            <div className="lg:overflow-y-auto">
               <div className="py-7 px-1">
-                <div className="h-[28rem] w-full">
+                <div className="lg:h-[28rem] h-auto w-full">
                   <video
                     autoPlay
                     muted
@@ -98,7 +98,7 @@ const ProjectPreview = ({
                   </video>
                 </div>
 
-                <div className="space-y-10 mt-14">
+                <div className="space-y-10 lg:mt-14 mt-5">
                   {selectedProject.images.map((img, i) => (
                     <div
                       key={i}
