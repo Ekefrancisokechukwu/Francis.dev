bb573b2432e587cc922e6d46ecc2d76284510c9f
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-01-14 06:18:24 +0100

    fix: unused state removed

--- src/app/components/navbars/SecondNav/index.tsx
+++ src/app/components/navbars/SecondNav/index.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import Showcase from "../../Showcase";
 import { codeSample } from "./codeSample";
 import Preview from "./Preview";
 
 const SecondNav = () => {
-  const [isMounted, setIsMounted] = useState(false);
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
   return (
     <div>
       <Showcase
