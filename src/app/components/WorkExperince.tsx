e884813ffc26b76395898f3b6846f2ffaf442cc4
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-01-15 12:11:01 +0100

    update: download resume to view

--- src/app/components/WorkExperince.tsx
+++ src/app/components/WorkExperince.tsx
@@ -96,7 +96,7 @@ const WorkExperince = () => {
         target="_blank"
         className="inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60 dark:bg-zinc-800/50 dark:text-zinc-300 dark:hover:bg-zinc-800 dark:hover:text-zinc-50 dark:active:bg-zinc-800/50 dark:active:text-zinc-50/70 group mt-6 w-full relative"
       >
-        Download Resume
+        View Resume
         <div className="relative overflow-hidden w-[15px] h-[15px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
