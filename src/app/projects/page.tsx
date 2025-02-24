3edb3ac48f4e2a054f2ef5ab4f91e05abced2549
Francis Okechukwu <113605646+Ekefrancisokechukwu@users.noreply.github.com>
2025-02-24 16:15:45 +0100

    update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Heading from "../components/Heading";
 import ProjectPreview from "./Preview";
 import { useState } from "react";
+import { projects } from "./data";
 
 export type ProjectProps = {
   title: string;
@@ -18,83 +19,6 @@ export type ProjectProps = {
   video: string;
   tools: string[];
 };
-
-const projects: ProjectProps[] = [
-  {
-    title: "synchub",
-    des: "A customizable portfolio platform similar to Linktree, allowing users to create a personalized page showcasing their information and important links.",
-    img: "/sy.png",
-    link: {
-      href: "https://synchub.vercel.app/",
-      label: "synchub",
-    },
-    githubLink: "https://github.com/Ekefrancisokechukwu/Synchub",
-    images: ["/lin-2.png", "/lin-1.png", "/lin-3.png", "/lin-4.png"],
-    video: "/sync_v.mp4",
-    tools: [
-      "Shadcn UI",
-      "Tailwind Css",
-      "Next.js",
-      "Typescript",
-      "Clerk AUTH",
-      "Zustand",
-      "Convex DB",
-      "Vercel Deploy",
-    ],
-  },
-  {
-    title: "Metrix Dashboard",
-    des: "A beautiful UI analytics dashboard that is highly responsive and well-optimized, providing a seamless user experience across various devices.",
-    img: "/default.avif",
-    link: {
-      href: "https://metrix-dashboard-ten.vercel.app/",
-      label: "Metrix",
-    },
-    video: "/met_v.mp4",
-    githubLink: "https://github.com/Ekefrancisokechukwu/metrix-dashboard",
-    images: ["/met-1.png"],
-    tools: ["Rechart js", "Next.js", "Javascript", "Styled components"],
-  },
-  {
-    title: "Sythensis Landing page",
-    des: "Synthetix offers AI-powered data analytics, automation, and visualization solutions for industries like healthcare, finance, and retail. With over 12 years of experience and a high client satisfaction rate, it helps businesses optimize operations using advanced AI insights.",
-    img: "/Synthetix.png",
-    link: {
-      href: "https://synthesis-two.vercel.app/",
-      label: "Sythensis",
-    },
-    video: "/web3_v.mp4",
-    githubLink: "https://github.com/Ekefrancisokechukwu/synthesis",
-    images: ["/sy-1.png", "/sy-2.png"],
-    tools: ["Next.js", "Framer motion", "Javascript", "Tailwind Css"],
-  },
-  {
-    title: "Specter Store",
-    des: "An e-commerce website offering a seamless shopping experience with a wide range of products, secure checkout, and user-friendly interface.",
-    img: "/default.avif",
-    link: {
-      href: "https://specter-store.vercel.app/",
-      label: "Specter",
-    },
-    video: "/spec-v.mp4",
-    githubLink: "https://github.com/Ekefrancisokechukwu/Specter_store",
-    images: [
-      "/spec-1.png",
-      "/spec-2.png",
-      "/spec-3.png",
-      "/spec-4.png",
-      "/spec-5.png",
-    ],
-    tools: [
-      "Next.js",
-      "Framer motion",
-      "Javascript",
-      "Tailwind Css",
-      "Redux Toolkit",
-      "Shadcn UI",
-    ],
-  },
-];
 
 const ProjectsPage = () => {
   const [isPreviewOpen, setIspreviewOpen] = useState(false);
